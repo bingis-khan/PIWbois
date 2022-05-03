f80750f6899e242fb92aef550889db7c0f226ed0
bingis-khan <robert.noparlik@gmail.com>
2022-05-03 11:55:15 +0200

    Fix all routing problems, nya

--- 3/std-prjs/src/index.js
+++ 3/std-prjs/src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/PIWbois/3/std-prjs/build/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
